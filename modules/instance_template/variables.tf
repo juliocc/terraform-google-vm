48bf6e88a2729444039e41f92fd3867e757e2737
Julio Castillo <juliocc@gmail.com>
2020-01-30 12:09:29 +0100

    make sa optional

--- modules/instance_template/variables.tf
+++ modules/instance_template/variables.tf
@@ -141,11 +141,8 @@ variable "metadata" {
 ##################
 
 variable "service_account" {
-  type = object({
-    email  = string
-    scopes = set(string)
-  })
   description = "Service account to attach to the instance. See https://www.terraform.io/docs/providers/google/r/compute_instance_template.html#service_account."
+  default = ""
 }
 
 ###########################
